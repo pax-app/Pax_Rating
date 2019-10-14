188224269049e68979422f4319321ca748f9faa9
Lucas Dutra <ldutra98@gmail.com>
2019-10-14 01:18:06 -0300

    pax-app/Wiki#164 Create provider service review arithmetic mean route
    Co-authored-by: Matheus Pimenta <ultra10vascaino@gmail.com>

--- project/api/views.py
+++ project/api/views.py
@@ -34,7 +34,7 @@ def get_users_review_average(evaluated_id):
         if not reviews:
             return jsonify(createFailMessage('Review not found for this user')), 404
 
-        user_average = 0
+        user_average = 0.0
         review_quantity = 0
 
         for review in reviews:
@@ -51,5 +51,35 @@ def get_users_review_average(evaluated_id):
 
     except ValueError:
         return jsonify(createFailMessage('User not found')), 404
+
+    return jsonify(response), 200
+
+@service_blueprint.route('/service_reviews/average/<evaluated_id>', methods=['GET'])
+def get_provider_service_review_average(evaluated_id):
+    response = {}
+
+    try:
+        service_reviews = Service.query.filter_by(evaluated_id=int(evaluated_id))
+
+        if not service_reviews:
+            return jsonify(createFailMessage('Service review not found for this provider')), 404
+
+        provider_average = 0.0
+        service_review_quantity = 0
+
+        for service_review in service_reviews:
+            provider_average += float(service_review.service_rate)
+            service_review_quantity += 1
+
+        provider_average = provider_average / service_review_quantity
+
+        response = {
+            'status': 'success',
+            'provider_id': evaluated_id,
+            'provider_service_review_average': provider_average
+        }
+
+    except ValueError:
+        return jsonify(createFailMessage('Proivider not found')), 404
 
     return jsonify(response), 200
